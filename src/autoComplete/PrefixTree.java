464462fd08f73d5e4b02205e48a5d56861d219d1
Clarissa Sigale <156869815+ClarissaSigale@users.noreply.github.com>
2025-04-08 12:49:06 -0500

    Attempted add and contains

--- src/autoComplete/PrefixTree.java
+++ src/autoComplete/PrefixTree.java
@@ -1,20 +1,21 @@
 package autoComplete;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
- * A prefix tree used for autocompletion. The root of the tree just stores links to child nodes (up to 26, one per letter).
- * Each child node represents a letter. A path from a root's child node down to a node where isWord is true represents the sequence
- * of characters in a word.
+ * A prefix tree used for autocompletion. The root of the tree just stores links to child nodes (up
+ * to 26, one per letter). Each child node represents a letter. A path from a root's child node down
+ * to a node where isWord is true represents the sequence of characters in a word.
  */
 public class PrefixTree {
-    private TreeNode root; 
+    private TreeNode root;
 
     // Number of words contained in the tree
     private int size;
 
-    public PrefixTree(){
+    public PrefixTree() {
         root = new TreeNode();
     }
 
@@ -24,35 +25,77 @@ public class PrefixTree {
      * @param word
      */
     public void add(String word){
-        //TODO: complete me
+        if (!contains(word)){
+            size++;
+            TreeNode tempRoot = root;
+            for (int i = 0; i < word.length(); i++){
+                
+                Character curr = word.charAt(i);
+                if (tempRoot.children.containsKey(curr)){
+                    if (i==word.length()-1){
+                        tempRoot.children.get(curr).isWord = true;
+                        return;
+                    }
+                    else{
+                        tempRoot = tempRoot.children.get(curr);
+                    }
+                }
+                else{
+                    tempRoot.children.put(curr, new TreeNode());
+                    if (i==word.length()-1){
+                        tempRoot.children.get(curr).isWord = true;
+                        return;
+                    }
+                    else{
+                        tempRoot = tempRoot.children.get(curr);
+                    }
+
+                }    
+            }
+        }
     }
 
     /**
      * Checks whether the word has been added to the tree
+     * 
      * @param word
      * @return true if contained in the tree.
      */
-    public boolean contains(String word){
-        //TODO: complete me
-        return false;
+    public boolean contains(String word) {
+        TreeNode tempRoot = root;
+        for (int i = 0; i < word.length(); i++) {
+            Character curr = word.charAt(i);
+            if (tempRoot.children.containsKey(curr)) {
+                if (i == word.length() - 1 && tempRoot.children.get(curr).isWord) {
+                    return true;
+                } else if (i == word.length() - 1) {
+                    return false;
+                }
+                tempRoot = tempRoot.children.get(curr);
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
-     * Finds the words in the tree that start with prefix (including prefix if it is a word itself).
-     * The order of the list can be arbitrary.
+     * Finds the words in the tree that start with prefix (including prefix if it is a word itself). The
+     * order of the list can be arbitrary.
+     * 
      * @param prefix
      * @return list of words with prefix
      */
-    public ArrayList<String> getWordsForPrefix(String prefix){
-        //TODO: complete me
+    public ArrayList<String> getWordsForPrefix(String prefix) {
+        // TODO: complete me
         return null;
     }
 
     /**
      * @return the number of words in the tree
      */
-    public int size(){
+    public int size() {
         return size;
     }
-    
+
 }
